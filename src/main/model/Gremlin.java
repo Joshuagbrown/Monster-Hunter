94d36483f0724df3758f6d322c9abd1e014b4b7f
Aimee Cloutman <aac66@uclive.ac.nz>
2022-05-20 06:33:40 +0000

    Update Gremlin.java - Javadoc

--- src/main/model/Gremlin.java
+++ src/main/model/Gremlin.java
@@ -1,5 +1,15 @@
 package main.model;
+
+/** 
+Class of a Gremlin which extends the monster class, holds all the values specific to Gremlin
+@author Aimée Cloutman
+*/
 public class Gremlin extends Monster { 
+
+/** 
+Holds all the specific statistics relevant to a Gremlin
+@author Aimée Cloutman
+*/
 	 public Gremlin() {
 		 //name, maxHealth, damage, healAmount, buyPrice, sellPrice 
 		 super("Gremlin", 30, 5, 5, 50, 50);
