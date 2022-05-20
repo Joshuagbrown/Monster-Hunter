63862bbd3f5334d09e4ffa135e417f24d9b40166
Aimee Cloutman <aac66@uclive.ac.nz>
2022-05-20 06:30:33 +0000

    Update GameRunner.java - JavaDoc

--- src/main/controller/GameRunner.java
+++ src/main/controller/GameRunner.java
@@ -13,10 +13,19 @@ import main.view.SetupScreen;
 import main.view.ShopScreen;
 import main.view.WinScreen;
 
+/**
+Runs and controls the main screens in the game
+@author Josh Brown
+*/
 public class GameRunner {
 	
 	GameEnvironment gameEnvironment;
-	
+
+/**
+Sets up a new game environment and adds the starting game available monsters to an array list
+@param gameEnvironment a new object of the game environment class
+@author Josh Brown
+*/
 	public static void setup(GameEnvironment gameEnvironment) {
 		ArrayList<Monster> startingList = new ArrayList<Monster>();
 		startingList.add(new Orc());
@@ -24,24 +33,47 @@ public class GameRunner {
 		startingList.add(new Human());
 		SetupScreen setupScreen = new SetupScreen(gameEnvironment, startingList);
 	}
-	
+
+/**
+Takes the current game environment and launches the main screen of the game for the player to view
+@param gameEnvironment the current state of the game environment 
+@author Josh Brown
+*/
 	public static void launchMainScreen(GameEnvironment gameEnvironment) {
 		MainScreen mainScreen = new MainScreen(gameEnvironment);
 	}
 	
+/** 
+Takes the current game environment information and launches the shop screen for the player to view
+@param gameEnvironment the current state of the game environment
+@author Josh Brown
+*/
 	public static void launchShopScreen(GameEnvironment gameEnvironment) {
 		ShopScreen shopScreen = new ShopScreen(gameEnvironment);
 	}
-	
+
+/** 
+Takes the current game environment information and launches the battle screen for the player to view
+@param gameEnvironment the current state of the game environment
+@author Josh Brown
+*/
 	public static void launchBattleScreen(GameEnvironment gameEnvironment) {
 		BattleScreen battleScreen = new BattleScreen(gameEnvironment);	
 	}
-	
+
+/**
+Displays the win screen for when a player wins a battle
+@author Josh Brown
+*/
 	public static void displayWin() {
 		WinScreen WinScreen = new WinScreen();	
 
 	}
 	
+/**
+Displays the loss screen for when a player loses a battle
+@author Josh Brown
+*/
 	public static void displayLoss() {
 		LossScreen lossScreen = new LossScreen();	
 
