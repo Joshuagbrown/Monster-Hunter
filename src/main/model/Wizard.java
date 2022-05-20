8d82d6d501622b1fc24be3dd24958c9163f05937
Aimee Cloutman <aac66@uclive.ac.nz>
2022-05-20 06:35:38 +0000

    Update Wizard.java - Javadoc

--- src/main/model/Wizard.java
+++ src/main/model/Wizard.java
@@ -1,5 +1,15 @@
 package main.model;
+
+/** 
+Class of a Wizard which extends the monster class, holds all the values specific to Wizard
+@author Aimée Cloutman
+*/
 public class Wizard extends Monster { 
+
+/** 
+Holds all the specific statistics relevant to a Wizard
+@author Aimée Cloutman
+*/
 	 public Wizard() {
 		 //name, maxHealth, damage, healAmount, buyPrice, sellPrice 
 
