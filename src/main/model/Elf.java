1fe90f1b6bc2b20d93131b2edf172d60c6d37244
Aimee Cloutman <aac66@uclive.ac.nz>
2022-05-20 06:32:08 +0000

    Update Elf.java - Javadoc

--- src/main/model/Elf.java
+++ src/main/model/Elf.java
@@ -1,7 +1,15 @@
 package main.model;
 
+/**
+Class of an Elf which extends the monster class, holds all the values specific to Elf
+@author Josh Brown
+*/
 public class Elf extends Monster {
 
+/**
+Holds all the specific statistics relevant to an Elf
+@author Josh Brown
+*/
 	public Elf() {
 		 //name, maxHealth, damage, healAmount, buyPrice, sellPrice 
 		super("Elf", 50, 10, 5, 50, 50);
