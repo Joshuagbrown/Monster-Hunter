12d9d43fbaff1ab6ce870d0f23d4a821c90e09a7
Aimee Cloutman <aac66@uclive.ac.nz>
2022-05-22 11:51:39 +0000

    Update StrengthPotion.java

--- src/main/model/StrengthPotion.java
+++ src/main/model/StrengthPotion.java
@@ -6,6 +6,10 @@ Class for the item option of a strength potion, it extends the item class as is 
 */
 public class StrengthPotion extends Item{
 
+/**
+Method holds all the specific characteristics of a Strength Potion
+@author Josh Brown
+*/
 	public StrengthPotion() {
 		//String description, int itemId, int purchasePrice, int sellPrice
 		super("Strength Potion", "Add 25% damage", 1, 300, 150);
